b46f755c52dbec2230e69e0d6bd971f57f6d1ed0
yeeann9912629 <182805L@mymail.nyp.edu.sg>
2021-01-03 20:53:04 +0800

    Update 03/01/2021

---
+++ views/js/index.js
@@ -0,0 +1,11 @@
+if (sessionStorage.getItem("login") != null)
+{
+    $(".navigation").append(`
+        <ul>
+            <li>Home</li>
+            <li>Test</li>
+            <li>Profile</li>
+            <li>Log Out</li>
+        </ul>
+    `);
+}
